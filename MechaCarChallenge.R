ce47226484063db9e70c4636a2b249399e1ca541
Ian T <68198233+ianakt@users.noreply.github.com>
2022-03-27 09:46:10 -0400

    Update MechaCarChallenge.R
    Added S_Coil csv

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -2,6 +2,8 @@ library(dplyr)
 library(knitr)
 library(ggplot2)
 MPGData <- read.csv(file = "MechaCar_mpg.csv")
+S_Coil <- read.table(file="Suspension_Coil.csv",sep=",",header = TRUE)
+
 Linear_Model_MPG<-lm(formula = mpg~vehicle_length
                      +vehicle_weight
                      +spoiler_angle
