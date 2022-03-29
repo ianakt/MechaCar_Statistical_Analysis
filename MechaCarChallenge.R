4bb116d5ad60a471d4c502cb911270713f9caf79
Ian T <68198233+ianakt@users.noreply.github.com>
2022-03-29 19:21:27 -0400

    Update MechaCarChallenge.R
    Completed code.... though mean of all might be wrong, is it PSI~1?

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -4,6 +4,7 @@ library(ggplot2)
 MPGData <- read.csv(file = "MechaCar_mpg.csv")
 S_Coil <- read.table(file="Suspension_Coil.csv",sep=",",header = TRUE)
 
+# Linear Model of mpg compared to all other factors
 Linear_Model_MPG<-lm(formula = mpg~vehicle_length
                      +vehicle_weight
                      +spoiler_angle
@@ -14,23 +15,12 @@ Linear_Model_MPG<-lm(formula = mpg~vehicle_length
 Lin_Mod_Mpg_Summary = summary(Linear_Model_MPG)
 Lin_Mod_Mpg_Summary
 
-
 total_summary <- S_Coil %>% 
   summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
 
 total_summary
 
-Lot11 <- S_Coil %>% filter(Manufacturing_Lot == "Lot1") # = Lot 1
-Lot22 <- S_Coil %>% filter(Manufacturing_Lot == "Lot2") # = Lot 2
-Lot33 <- S_Coil %>% filter(Manufacturing_Lot == "Lot3") # = Lot 3
-
-Lot_1 <- subset(S_Coil, Manufacturing_Lot == "Lot1" ,select = PSI)
-Lot_2 <- subset(S_Coil, Manufacturing_Lot == "Lot2" ,select = PSI)
-Lot_3 <- subset(S_Coil, Manufacturing_Lot == "Lot3" ,select = PSI)
-
-
-t.test(Lot11$PSI, Lot22$PSI, Lot33$Psi,  paired = TRUE)
-
+# Lot Summary
 lot_summary <- S_Coil %>% 
   group_by(Manufacturing_Lot) %>%
   summarize(Mean=mean(PSI)
@@ -41,29 +31,38 @@ lot_summary <- S_Coil %>%
 lot_summary
 
 
+# grouping lots by subsets didn't work in the function for me
+#PSI_Subset <- subset(S_Coil, select = PSI)
+
+#Lot_1 <- subset(S_Coil, Manufacturing_Lot == "Lot1" ,select = PSI)
+#Lot_2 <- subset(S_Coil, Manufacturing_Lot == "Lot2" ,select = PSI)
+#Lot_3 <- subset(S_Coil, Manufacturing_Lot == "Lot3" ,select = PSI)
 
 
-# # Experimental Below
+#PSI_All <- t.test(formula = PSI~1,data = S_Coil)
+#PSI_All
 
-# PSI_Subset <- subset(S_Coil, select = PSI)
+#PSI_All2 <- t.test(formula = PSI~1,data = S_Coil, mu = 1498.78)
+#PSI_All2
 
-# # Type Error when using t-test
+# Lots grouped by filter
 
-
-
-# #These t-test show output, though ave different results
-
-
-# PSI_All <- t.test(formula = PSI~1,data = S_Coil)
-# PSI_All
-
-# PSI_All2 <- t.test(formula = PSI~1,data = S_Coil, mu = 1498.78)
-# PSI_All2
+Lot11 <- S_Coil %>% filter(Manufacturing_Lot == "Lot1")
+Lot22 <- S_Coil %>% filter(Manufacturing_Lot == "Lot2")
+Lot33 <- S_Coil %>% filter(Manufacturing_Lot == "Lot3")
 
 
+# Difference of means
+t.test(Lot11$PSI, Lot22$PSI, Lot33$PSI,  paired = TRUE)
 
-# # This didn't work, modelled from mod 15.6.3 15.6.4
+Lot1v2 <- t.test(Lot11$PSI, Lot22$PSI,  paired = TRUE)
+Lot2v3 <- t.test(Lot22$PSI, Lot33$PSI,  paired = TRUE)
+Lot1v3 <- t.test(Lot11$PSI, Lot33$PSI,  paired = TRUE)
 
-# t.test(formula = Lot11~PSI,data = S_Coil)
-# t.test(formula = Lot11~1,data = S_Coil)
+Lot1v2
+Lot2v3
+Lot1v3
+
+
+
 
